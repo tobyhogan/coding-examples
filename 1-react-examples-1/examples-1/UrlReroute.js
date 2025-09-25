ed51de02f0ed7a6735d4edc1d78c6b2ebda2bbe9
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2025-09-25 11:34:30 +0100

    .

---
+++ 1-react-examples-1/examples-1/UrlReroute.js
@@ -0,0 +1,27 @@
+    
+    
+    
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}
+    >
+      <div className="min-h-screen bg-gray-50">
+
+        <Header />
+
+        <main className="container mx-auto px-4 py-8">
+          
+          <Routes>
+
+            <Route path="/" element={<Navigate to="/page1" replace />} />
+
+            <Route path="/page1" element={<Page1 />} />
+            <Route path="/page2" element={<Page2 />} />
+
+          </Routes>
+
+        </main>
+      </div>
+    </Router>
